46d2bf9ecb835a318da43af6e813dc709545d93c
RITUYAOkamoto <116532348+1206354@users.noreply.github.com>
2022-11-28 11:10:57 +0900

    import文の追加

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'use strict';
+import {PythonShell} from 'python-shell';
+
+let options = {
+  mode: 'text',
+  pythonPath: 'path/to/python',
+  pythonOptions: ['-u'], // get print results in real-time
+  scriptPath: 'path/to/my/scripts',
+  args: ['value1', 'value2', 'value3']
+};
+
+PythonShell.run('my_script.py', options, function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution
+  console.log('results: %j', results);
+});
 
 const express = require('express');
 const line = require('@line/bot-sdk');
